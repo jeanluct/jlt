5a712f9871f42981f510ebf5e69c08b2890a5533
jeanluc <jeanluc@f2730527-01e6-0310-8412-a4dd2dbf00c1>
2010-05-29 03:59:50 +0000

    * Vast improvement to refine/refine2.  Works like fft/fft2 now.  There was a bug in the high modes for even dimenions.  * refine2_old.m is basically the old code revamped.  Ditch.

--- matlab/refine.m
+++ matlab/refine.m
@@ -1,28 +1,54 @@
-function fp = refine(f,N)
+function fp = refine(f,N,dim)
 %REFINE   Increase resolution of an array using FFT.
-%   REFINE(F,N) returns F inflated to length N by taking the FFT of
-%   the data, padding with zeros, then inverting using the IFFT.
+%   REFINE(F,N) returns F inflated to length N by taking the FFT of the
+%   data, padding with zeros, then inverting using the IFFT.  For matrices,
+%   the refine operation is applied to each column. For N-D arrays, refine
+%   operates on the first non-singleton dimension.
+%
+%   REFINE(F,N,DIM) applies the refine operation across the dimension DIM.
 %
 %   See also REFINE2.
 
-N0 = length(f);
+% TODO: Downsampling.
 
-if N < N0
-  error('No downsampling in refine')
+if nargin < 3
+  % First non-singleton dimension
+  dim = min(find(size(f) ~= 1));
+  if isempty(dim), dim = 1; end
 end
 
-kmin0 = floor(-(N0-1)/2);
-kmax0 = floor( (N0-1)/2);
-kmin = floor(-(N-1)/2);
-kmax = floor( (N-1)/2);
-c = -kmin+1;
-c0 = -kmin0+1;
-k0 = [kmin0:kmax0];
+sz = size(f);
+N0 = sz(dim);
+if sz(dim) > N
+  error('No downsampling in refine');
+elseif sz(dim) == N
+  fp = f; return
+end
+sz(dim) = N;
+fk = zeros(sz);
 
-fk0 = fftshift(fft(f));
-fk = zeros(N,1);
+kmin0 = floor(-(N0-1)/2); kmax0 = floor( (N0-1)/2);
+kmin = floor(-(N-1)/2); k0 = [kmin0:kmax0];
 
-% Will this work with even N0?  Then we need to duplicate the Nyquist mode.
-fk(c + k0) = fk0(c0 + k0);
+% Create vector of indices along each dimension and store in cell.
+numDims = ndims(f);
+ic = cell(1,numDims);
+for k = 1:numDims
+  nd = size(fk,k);
+  if k ~= dim
+    ic{k} = 1:nd;
+  else
+    ic{k} = k0-kmin+1;   % The dimension we operate on.
+  end
+end
 
-fp = (N/N0)*ifft(ifftshift(fk),'symmetric');
+fk(ic{:}) = fftshift(fft(f,[],dim),dim);
+
+% When N0 is even, need to copy the Nyquist mode.
+if ~mod(N0,2)
+   ic{dim} = 1-kmin - kmin0;
+   ic2 = ic; ic2{dim} = 1-kmin + kmin0;
+   fk(ic{:}) = conj(fk(ic2{:}));
+end
+
+fp = (N/N0)*ifft(ifftshift(fk,dim),[],dim,'symmetric');
--- matlab/refine2.m
+++ matlab/refine2.m
@@ -1,29 +1,14 @@
-function fp = refine2(f,N)
+function fp = refine2(f,M,N)
 %REFINE2   Increase resolution of a 2D graph using FFT.
-%   REFINE2(F,N) returns an N by N version of F, inflated
-%   by taking the FFT of the data, padding with zeros, then
-%   inverting using the IFFT.
+%   REFINE2(F,M,N) returns an M by N version of the 2D array F, inflated by
+%   taking the FFT of the data, padding with zeros, then inverting using the
+%   IFFT.
+%
+%   REFINE2(F,N) returns an N by N array.
 %
 %   See also REFINE.
 
-N0 = size(f,1);
+if nargin < 3, N = M; end
 
-if N < N0
-  error('No downsampling in refine2')
-end
-
-kmin0 = floor(-(N0-1)/2);
-kmax0 = floor( (N0-1)/2);
-kmin = floor(-(N-1)/2);
-kmax = floor( (N-1)/2);
-c = -kmin+1;
-c0 = -kmin0+1;
-k0 = [kmin0:kmax0];
-
-fk0 = fftshift(fft2(f));
-fk = zeros(N,N);
-
-% Will this work with even N0?  Then we need to duplicate the Nyquist mode.
-fk(c + k0,c + k0) = fk0(c0 + k0,c0 + k0);
-
-fp = (N/N0)^2*ifft2(ifftshift(fk),'symmetric');
+% Apply refine to each dimension.  (See doc fft.)
+fp = refine(refine(f,M,1).',N,1).';
